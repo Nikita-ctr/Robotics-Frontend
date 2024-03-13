e5f3a868816c70fe5edc0f2316981a5dae9f6454
yorrdt <this.mazahaka@gmail.com>
2024-03-13 15:33:32 +0300

    make gradient more in about_laboratory

--- src/components/ProjectHexagon/ProjectHexagon.tsx
+++ src/components/ProjectHexagon/ProjectHexagon.tsx
@@ -9,8 +9,15 @@ interface HexagonProps {
 const ProjectHexagon = ({ index, text }: HexagonProps) => {
     return (
         <div className={styles.hexagon_container}>
-            <div className={styles.hexagon_image}>
-                <Image key={index} src={`/images/projects_heaxagon/hexagon_${index + 1}.png`} alt="" height={224} width={198} />
+            <div className={styles.hexagon_image_container}>
+                <Image
+                    className={styles.hexagon_image}
+                    key={index}
+                    src={`/images/projects_heaxagon/hexagon_${index + 1}.png`}
+                    alt=""
+                    height={224}
+                    width={198}
+                />
                 <div className={styles.hexagon_text}>{text}</div>
             </div>
         </div>
